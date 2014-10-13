521251a85f76312ff426b53679b6d7b678fc9cec
Kazuhiro Sera <seratch@gmail.com>
2014-10-13 12:37:05 +0900

    Fix comments

--- src/main/scala/hackernews4s/v0/HackerNews.scala
+++ src/main/scala/hackernews4s/v0/HackerNews.scala
@@ -5,8 +5,14 @@ import skinny.http._
 import skinny.logging.Logging
 import skinny.util.JSONStringOps._
 
+/**
+ * HackerNews API client
+ */
 object HackerNews extends HackerNews
 
+/**
+ * HackerNews API client
+ */
 trait HackerNews extends Logging {
 
   val BASE_URL = "https://hacker-news.firebaseio.com/v0"
@@ -15,6 +21,7 @@ trait HackerNews extends Logging {
 
   /**
    * Items
+   *
    * Stories, comments, jobs, Ask HNs and even polls are just items.
    * They're identified by their ids, which are unique integers, and live under https://hacker-news.firebaseio.com/v0/item/.
    */
@@ -29,6 +36,7 @@ trait HackerNews extends Logging {
 
   /**
    * Users
+   *
    * Users are identified by case-sensitive ids, and live under https://hacker-news.firebaseio.com/v0/user/.
    */
   def getUser(userId: UserId): Option[User] = {
@@ -42,6 +50,7 @@ trait HackerNews extends Logging {
 
   /**
    * Top Stories
+   *
    * The current top 100 stories are at https://hacker-news.firebaseio.com/v0/topstories.
    */
   def getItemIdsForTopStories(): Seq[ItemId] = {
@@ -54,7 +63,7 @@ trait HackerNews extends Logging {
   }
 
   /**
-   * Top Stories (Actual Items)
+   * Retrieves actual top stories.
    */
   def getTopStories(limit: Int = 10): Seq[Item] = {
     getItemIdsForTopStories().take(limit).grouped(CONCURRENCY).flatMap(ids =>
@@ -64,6 +73,7 @@ trait HackerNews extends Logging {
 
   /**
    * Max Item ID
+   *
    * The current largest item id is at https://hacker-news.firebaseio.com/v0/maxitem.
    */
   def getMaxItemId(): ItemId = {
@@ -81,6 +91,7 @@ trait HackerNews extends Logging {
 
   /**
    * Changed Items and Profiles
+   *
    * The item and profile changes are at https://hacker-news.firebaseio.com/v0/updates.
    */
   def getIdsForChangedItemsAndProfiles(): ChangedItemsAndProfiles = {
@@ -95,7 +106,7 @@ trait HackerNews extends Logging {
   }
 
   /**
-   * Retrieve changed item data for ids from "Changed Items and Profiles API".
+   * Retrieves actual changed item data for ids from "Changed Items and Profiles API".
    */
   def getChangedItems(): Seq[Item] = {
     getIdsForChangedItemsAndProfiles().itemIds.grouped(CONCURRENCY).flatMap(ids =>
@@ -104,7 +115,7 @@ trait HackerNews extends Logging {
   }
 
   /**
-   * Retrieve changed user data for ids from "Changed Items and Profiles API".
+   * Retrieves actual changed user data for ids from "Changed Items and Profiles API".
    */
   def getChangedProfiles(): Seq[User] = {
     getIdsForChangedItemsAndProfiles().userIds.grouped(CONCURRENCY).flatMap(ids =>
@@ -112,9 +123,10 @@ trait HackerNews extends Logging {
     ).toSeq
   }
 
+  // creates skinny.http.Request for each API call
   private[this] def req(url: String): Request = {
     val req = new Request(url)
-    req.connectTimeoutMillis(2000) // HackerNews API sometimes rejects connections
+    req.connectTimeoutMillis(2000) // HackerNews API sometimes cannot accept new connection
     req
   }
 
